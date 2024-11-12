34aa636bc3770e9570dd5a53f7f777a4cdd6bc14
Joris Bertier <joris.bertier@gmail.com>
2024-11-12 11:00:26 -0600

    create component AcoountCard

--- src/components/AccountCard.tsx
+++ src/components/AccountCard.tsx
@@ -3,15 +3,15 @@ import React from 'react'
 type Props = {
     title: string;
     amount: string;
-    description: string;
 }
-function AccountCard({title, amount, description}: Props) {
+
+function AccountCard({title, amount}: Props) {
     return (
         <section className="account">
         <div className="account-content-wrapper">
         <h3 className="account-title">{title}</h3>
         <p className="account-amount">{amount}</p>
-        <p className="account-amount-description">{description}</p>
+        <p className="account-amount-description">Available Balance</p>
         </div>
         <div className="account-content-wrapper cta">
         <button className="transaction-button">View transactions</button>
