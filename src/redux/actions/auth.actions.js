82723d89bb5bf78112c06a194c5f44d8724d31f1
Joris Bertier <joris.bertier@gmail.com>
2024-12-12 08:46:21 -0600

    remove comment

--- src/redux/actions/auth.actions.js
+++ src/redux/actions/auth.actions.js
@@ -1,17 +1,4 @@
 import { fetchUserProfile } from './user.actions';
-// import axios from 'axios';
-
-// const API_BASE_URL = 'http://localhost:3001/api/v1/user';
-
-// export const signupUser = (userData) => async (dispatch) => {
-//     try {
-//         const response = await axios.post(`${API_BASE_URL}/signup`, userData);
-//         dispatch({ type: 'SIGNUP_SUCCESS', payload: response.data });
-//     } catch (error) {
-//         dispatch({ type: 'SIGNUP_ERROR', payload: error.response.data });
-//     }
-// };
-
 export const loginUser = (credentials) => async (dispatch) => {
         try {
         const response = await fetch("http://localhost:3001/api/v1/user/login", {
