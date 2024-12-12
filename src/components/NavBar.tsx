f01ff27b57507b4424edbeccc0299fd4db8ab682
Joris Bertier <joris.bertier@gmail.com>
2024-12-12 09:31:31 -0600

    remove import useless

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import ArgentLogo from '../assets/images/argentBankLogo.png';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
