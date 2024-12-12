678c6143e27207477a42d676f37afe48f0d12d36
Joris Bertier <joris.bertier@gmail.com>
2024-12-12 08:40:21 -0600

    change text

--- src/pages/Login/SignIn.tsx
+++ src/pages/Login/SignIn.tsx
@@ -39,7 +39,7 @@ function SignIn() {
                     <label htmlFor="password">Password</label>
                     <input type="password" id="password" onChange={(e) => setPassword(e.target.value)}/>
                 </div>
-                {errorMessage && <span className='error-message'>Email or password incorrecte</span> }
+                {errorMessage && <span className='error-message'>Email or password incorrect, please enter again</span> }
                 <div className="input-remember">
                     <input type="checkbox" id="remember-me"/>
                     <label htmlFor="remember-me">Remember me</label>
